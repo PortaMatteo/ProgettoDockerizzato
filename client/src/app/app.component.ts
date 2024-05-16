c95094fe558288e153e26a23d8e77d80eb65140c
PortaMatteo <porta.matteo@iisgalvanimi.edu.it>
2024-05-16 11:58:55 +0000

    fine 02 Adding a Python Server

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'client';
+  data: any;
+  constructor(private http: HttpClient)
+  {
+    this.http.get("https://5000-portamatteo-progettodoc-2clxqe14jhh.ws-eu111.gitpod.io/simple_json")
+    .subscribe(
+      (data) => 
+        {
+        this.data = data;
+        console.log(this.data)
+      }
+      
+    )
+    
+  }
 }
